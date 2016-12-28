35db6d4213d39cf88b0b13bdc2806df1d49644c5
Byron Chu <chu.byron@yahoo.com>
2016-12-28 15:45:22 +1100

    changed to use Express

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-// import http[ module
+/*
+
 
 var http = require('http')
 
@@ -16,3 +17,19 @@ var server = http.createServer(handleRequests);
 server.listen(8080,function(){
   console.log('Listening on port 8080');
 });
+*/
+///////  use express ///
+var express = require('express');
+var app = express();
+var port = 8080;
+
+//start server
+app.listen(port, function() {
+  console.log('app started');
+});
+
+// route out app
+
+app.get('/', function(req,res) {
+  res.send('hello world');  
+})
